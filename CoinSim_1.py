9132ea2c767d31e7ce4c06bed953460110b6c13b
ShreySuri <shreysuri5@gmail.com>
2022-07-16 11:56:12 -0500

    Create CoinSim_1.py

---
+++ CoinSim_1.py
@@ -0,0 +1,9 @@
+
+flips = 0
+
+while flips % 1 != 0 and flips < 1:
+	print("")
+	flips = input(print("How many times will the coun be flipped. Please enter an integer. "))
+	flips = float(flips)
+flips = int(flips)
+
