3c4e5be7e42b508cfdbc0c9059cad33802a32096
ShreySuri <shreysuri5@gmail.com>
2022-07-16 23:06:15 -0500

    Update CoinSim_1.py

--- CoinSim_1.py
+++ CoinSim_1.py
@@ -7,3 +7,10 @@ while flips % 1 != 0 and flips < 1:
 	flips = float(flips)
 flips = int(flips)
 
+total_events = 2 ** flips
+
+head_count = 0
+
+for  i in range (0, flips):
+	head_count = head_count + 1
+	opposite = flips - head_count
