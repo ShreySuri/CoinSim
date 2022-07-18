fca58d7c75f6474c4c9c84ef90f7bf2289a6bbae
ShreySuri <shreysuri5@gmail.com>
2022-07-18 08:35:45 -0500

    mostly done

--- CoinSim_2.py
+++ CoinSim_2.py
@@ -1,33 +1,30 @@
 def bin_list(x):
     list_1 = [0]
-    list_length = 1
-    val_count = 1
-    
+    power = 1
+    while 2 ** power <= x:
+        power = power + 1
+        list_1.append(0)
+
+    val_count = power
     for i in range (0, x):
-
-        j = i
-
-        while j > 1:
-            j = j/2
-
-        if j == 1 and i != i:
-            list_1.append(0)
-            val_count = val_count + 1
-        else:
-            toggle = True
-
         list_1[0] = list_1[0] + 1
-        for i in range (0, val_count):
-            if list_1[i] == 2:
-                list_1[i] = 0
-                x = i + 1
-                list_1[x] = list_1[x] + 1
 
-        return(list_1)
+        for j in range (0, val_count):
+            if list_1[j] == 2:
+                list_1[j] = 0
+                k = j + 1
+                list_1[k] = list_1[k] + 1
+            else:
+                toggle = True
 
-while flips % 1 != 0 and flips < 1:
+    list_1.reverse()
+    return(list_1)
+
+
+flips = 0
+while flips % 1 != 0 or flips < 1:
     print("")
-    flips = input(print("How many times will the coin be flipped. Please enter an integer. "))
+    flips = input(print("How many times will the coin be flipped? Please enter an integer. "))
     flips = float(flips)
 flips = int(flips)
 
