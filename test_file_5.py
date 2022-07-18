48835bd0340b5911e0000a333ffa467875d42a3d
ShreySuri <shreysuri5@gmail.com>
2022-07-17 20:48:42 -0500

    Update test_file_5.py

--- test_file_5.py
+++ test_file_5.py
@@ -1,7 +1,18 @@
-def stringToList(string):
-    listRes = list(string.split(""))
-    return listRes
+import math
 
 
-strA = "Millie Bobby Brown"
-print(stringToList(strA))
+def bin_list(x):
+    list_1 = [0]
+    for i in range (0, x):
+
+        j = i
+        while j > 1:
+            j = j/2
+        if j == 1:
+            list_1.append(0)
+        else:
+            toggle = True
+
+        
+            
+
