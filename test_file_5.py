272e080a4e32426fde0dd76e158e2345b48b3483
ShreySuri <shreysuri5@gmail.com>
2022-07-18 07:59:48 -0500

    Update test_file_5.py

--- test_file_5.py
+++ test_file_5.py
@@ -3,6 +3,7 @@ import math
 
 def bin_list(x):
     list_1 = [0]
+    list_length = 1
     for i in range (0, x):
 
         j = i
@@ -12,6 +13,13 @@ def bin_list(x):
             list_1.append(0)
         else:
             toggle = True
+        if i == 1:
+            list_1.remove(0)
+        else:
+            toggle = False
+
+        list_1[0] = list_1[0] + 1
+        for i in range (0, 
 
         
             
