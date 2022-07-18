c0adb0521ea7f2ac5a370e668dd99c302ce7e940
ShreySuri <shreysuri5@gmail.com>
2022-07-17 20:49:12 -0500

    Update CoinSim_2.py

--- CoinSim_2.py
+++ CoinSim_2.py
@@ -2,3 +2,20 @@ def binary(integer):
     bit_string = bin(integer)
     final = bit_string.lstrip('-0b')
     return(final)
+
+while flips % 1 != 0 and flips < 1:
+    print("")
+    flips = input(print("How many times will the coin be flipped. Please enter an integer. "))
+    flips = float(flips)
+flips = int(flips)
+
+total_events = 2 ** flips
+
+for i in range (0, total_events):
+    flip_pos = binary(i)
+    flip_pos = flip_pos.split()
+
+def bin_list(x):
+    list_1 = [0]
+    list_length = 1
+    for i in range (0, x):
